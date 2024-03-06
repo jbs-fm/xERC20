ff93cd1249ea40c770a25cbe7be68e17769631b0
jbs-fm <36034436+jbs-fm@users.noreply.github.com>
2024-03-06 15:37:34 +0000

    fix deploy

--- solidity/scripts/XERC20Deploy.sol
+++ solidity/scripts/XERC20Deploy.sol
@@ -38,7 +38,7 @@ contract XERC20Deploy is Script, ScriptingLibrary {
   using stdJson for string;
 
   uint256 public deployer = vm.envUint('DEPLOYER_PRIVATE_KEY');
-  XERC20Factory public factory = XERC20Factory(0xb913bE186110B1119d5B9582F316f142c908fc25);
+  XERC20Factory public factory = XERC20Factory(0x7673F15B0492A681CE57Db82b81E00D6b7797A11);
 
   function run() public {
     string memory _json = vm.readFile('./solidity/scripts/xerc20-deployment-config.json');
